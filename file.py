3772f83a0473f9973126af51293d882548a021b2
Legacy <82098783+Legacyww@users.noreply.github.com>
2023-08-31 22:29:46 +0300

    Delete file.py

--- file.py
+++
@@ -1,9 +0,0 @@
-import random
-list = ["Kate","Nick","Den","Alex","Tom"]
-new_list = []
-while len(new_list) != len(list):
-    i = random.randrange(0,len(list))
-    if list[i] not in new_list:
-        new_list.append(list[i])
-print(new_list)
-
